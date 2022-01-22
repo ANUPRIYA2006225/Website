1276d6632689577b0ccefcc39a81506458ed2b77
Anupriya <anujhapriya.2002@gmail.com>
2022-01-22 09:44:52 +0530

    web

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Android Developer","Web Developer","Shophomore",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
